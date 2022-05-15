d75edb9e2592d564e4cc6847d306714c87d996cb
Albert Ferran Casas <albert.ferran@gmail.com>
2022-05-15 18:22:59 +0200

    update package info

--- confmerge/__init__.py
+++ confmerge/__init__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
-__author__ = "Andre Lehmann"
-__email__ = "aisberg@posteo.de"
+__author__ = "Albert ferran"
+__email__ = "aferranc@cirsa.com"
 __version__ = '0.2.0'
 
 from confmerge.cli import cli
