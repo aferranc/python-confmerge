59a4b0d5f59a3b4676b6bda9fd0870a95ffb82ba
Albert Ferran Casas <aferranc@cirsa.com>
2022-05-16 15:37:05 +0200

    updated setup.py info

--- setup.py
+++ setup.py
@@ -2,45 +2,45 @@
 from setuptools import setup, find_packages
 from os import path
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
+with open(path.join(path.abspath(path.dirname(__file__)), "README.md")) as f:
     long_description = f.read()
 
 setup(
-    name='ConfMerge',
-    version='0.2.0',
-    author='Andre Lehmann',
-    author_email='aisberg@posteo.de',
-    url='https://github.com/Aisbergg/python-confmerge',
-    license='LGPL',
-    description='Configuration file merge utility',
+    name="python3-confmerge",
+    version="0.2.0",
+    author="Albert Ferran",
+    author_email="aferranc@cirsa.com",
+    url="https://github.com/aferranc/python-confmerge",
+    license="LGPL",
+    description="Configuration file merge utility",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords='config ini yaml json command-line CLI',
+    long_description_content_type="text/markdown",
+    keywords="config ini yaml json command-line CLI",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: System :: Systems Administration',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Bug Reports': 'https://github.com/Aisbergg/python-confmerge/issues',
-        'Source': 'https://github.com/Aisbergg/python-confmerge',
+        "Bug Reports": "https://github.com/aferranc/python-confmerge/issues",
+        "Source": "https://github.com/aferranc/python-confmerge",
     },
-    packages=find_packages(exclude=['examples']),
+    packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'confmerge = confmerge:cli',
+        "console_scripts": [
+            "confmerge = confmerge:cli",
         ]
     },
     install_requires=[
-        'pyyaml',
+        "pyyaml",
     ],
     include_package_data=True,
     zip_safe=True,
-    platforms=['POSIX'],
+    platforms=["POSIX"],
 )
